bfd8b8532897e1e49ed92bfd52a604c892e43d0f
gnkapet <gnkapet@gmail.com>
2025-08-21 10:12:58 +0000

    Deploying to gh-pages from @ gnkapet/gnkapet.github.io@daf6420ddb41a8e497712e938968d699a152bb8a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -515,7 +515,7 @@ ninja.data = [{
         title: 'ResearchGate',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.researchgate.net/profile/Giorgos-Kapetanakis/", "_blank");
+          window.open("https://www.researchgate.net/profile/https://www.researchgate.net/profile/Giorgos-Kapetanakis/", "_blank");
         },
       },{
         id: 'social-scholar',
