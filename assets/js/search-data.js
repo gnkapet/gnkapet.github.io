d888ce6ca0d2bed45433a78227c9e1f301026701
gnkapet <gnkapet@gmail.com>
2025-08-25 18:48:49 +0000

    Deploying to gh-pages from @ gnkapet/gnkapet.github.io@0aa317a3976c8a7d26da2254789d2a8637d37210 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Here you can find information and links related to my academic teaching activities.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
